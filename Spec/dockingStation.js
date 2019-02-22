88842be45e1acadc80ce72dce9ebf34e4586b7d0
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-22 15:28:17 +0000

    testing to dock a bike

--- Spec/dockingStation.js
+++ Spec/dockingStation.js
@@ -1,12 +1,20 @@
 describe("DockingStation", function() {
   var dockingStation;
+  var bike;
 
   beforeEach(function() {
     dockingStation = new DockingStation();
+    bike = jasmine.createSpy('bike');
   });
 
   it("should start with dockedbikes equating to an empty array", function() {
     expect(dockingStation.showDockedBikes()).toEqual([]);
   });
+
+  it("should be possible to dock a bike", function() {
+    dockingStation.dockBike(bike);
+    expect(dockingStation.showDockedBikes()).toEqual([bike]);
+  });
+
 
 });
